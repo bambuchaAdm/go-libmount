f5619119f470ee504eaea38b62e427298f783e54
Łukasz Dubiel <bambucha14@gmail.com>
2013-12-06 00:18:47 +0100

    Add AppendOption function.

--- context.go
+++ context.go
@@ -2,7 +2,18 @@ package context;
 
 //#cgo pkg-config: mount
 //#include <libmount/libmount.h>
+//#include <stdlib.h>
 import "C"
+
+import "unsafe"
+
+type LibmountError struct {
+	messages string
+}
+
+func (input LibmountError) Error() string {
+	return input.messages
+}
 
 type Context struct {
 	 handler * C.struct_libmnt_context
@@ -15,3 +26,15 @@ func createContext() (* Context) {
 func (context * Context) free() {
 	C.mnt_free_context(context.handler)
 }
+
+func (context * Context) AppendOptions(options string) (err error) {
+	cstring := C.CString(options)
+	defer C.free(unsafe.Pointer(cstring))
+	result := C.mnt_context_append_options(context.handler,cstring)
+	if result == 0 {
+		return nil;
+	} else {
+		return LibmountError{"Error on appending options to context"}
+	}
+}
+
